5e25866e200ccdd3fe35ac3635efdce1da051cd1
Darth Sidious <93368784+LordSithCanRa@users.noreply.github.com>
2023-09-13 20:02:59 +0200

    Publicando

---
+++ src/components/About.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduccion</p>
+        <h2 className={styles.sectionHeadText}>Vista previa.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Tengo buen nivel en torno a los lenguajes tipo TypeScript como JavaScript tambien he usado frameworks como React, Node.js, y
+        Three.js. También tengo experiencia en lenguaje orientado a Objetos con Java he usado frameworks como Vaadin, Spring y Spring Boot.
+        Tambien he programado en Oracle APEX un entorno nuevo web que incluye desarrollo en PL/SQL y tambien se puede combinar con codigo JavaScript y CSS.
+        Soy colaborativo, ayudo siempre que pueda a todos mis compañeros, y creo que mi mayor fuerte es que programar es mi PASIÓN.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
